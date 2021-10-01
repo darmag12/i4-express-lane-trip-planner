216c78139cab22195c0edfcfbc40bc012295044f
Daryl Magera <DarylMagera@global5.local>
2021-10-01 16:49:02 -0400

    added webpack config, babel and dotenv.

---
+++ data.js
@@ -0,0 +1,188 @@
+// List of all the westbound roads
+
+export const eastWestBounds = [
+  {
+    id: 'a',
+    title: 'Daytona Beach/DeLand/Lake Mary',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'b',
+    title: 'S.R. 434 (Mile 94)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'c',
+    title: 'Central Pkwy. (Mile 93)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'd',
+    title: 'S.R. 436 (Mile 92)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'e',
+    title: 'Maitland Blvd. (S.R. 414) (Mile 90)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'f',
+    title: 'Lee Road (S.R. 423) (Mile 88)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'g',
+    title: 'Fairbanks Ave. (Mile 87)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'h',
+    title: 'Par St. (Mile 86)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'i',
+    title: 'Princeton St. (Mile 85)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'j',
+    title: 'Ivanhoe Blvd. (Mile 84)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'k',
+    title: 'Colonial Drive (S.R. 50) (Mile 84)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'l',
+    title: 'Amelia St. (Mile 83)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'm',
+    title: 'South St. (Mile 82)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'n',
+    title: 'Anderson St. (Mile 82)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'o',
+    title: 'S.R. 408 (Mile 82)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'p',
+    title: 'Michigan St./Kaley St. (Mile 81)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'q',
+    title: 'Orange Blossom Trl. (US 441/17/92) (Mile 80)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'r',
+    title: 'John Young Pkwy. (Mile 79)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 's',
+    title: 'Conroy Road (Mile 78)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 't',
+    title: "Florida's Turnpike (Mile 77)",
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'u',
+    title: 'Grand National Drive (Mile 75)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'v',
+    title: 'Kirkman Road (S.R. 435) (Mile 75)',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+  {
+    id: 'w',
+    title: 'Tampa/Lakeland/Kissimmee',
+    coords: [
+      { eLat: 0, eLng: -1 },
+      { wLat: 0, wLng: -1 },
+    ],
+  },
+];
---
+++ src/app.js
@@ -0,0 +1,10 @@
+import { eastWestBounds } from '../data.js';
+
+// This is set to an environment variable
+// let appKey = ;
+
+// eastWestBounds.forEach(element => {
+//   console.log(element);
+// });
+const apiKey = process.env.API_KEY;
+console.log(apiKey);
