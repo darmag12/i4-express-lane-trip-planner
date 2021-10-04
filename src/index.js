1d27fd68c68efb59f9096327a178a5abf4b78bd9
Daryl Magera <mageradaryl12@gmail.com>
2021-10-04 11:22:18 -0400

    cleaned up some files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { eastWestBounds } from '../data.js';
+
+// This is set to an environment variable
+// let appKey = ;
+
+// eastWestBounds.forEach(element => {
+//   console.log(element);
+// });
+const apiKey = process.env.API_KEY;
+console.log(apiKey);
+console.log('Hi');
+// {
+//     "private": true,
+//     "scripts": {
+//       "start": "webpack serve",
+//       "watch": "webpack --watch",
+//       "build": "webpack"
+//     },
+//     "devDependencies": {
+//       "webpack": "^5.56.1",
+//       "webpack-cli": "^4.8.0",
+//       "webpack-dev-server": "^4.3.0"
+//     }
+//   }
