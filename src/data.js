3607ab2267933ede6c1f38b76fdc984ff8c418ef
Daryl Magera <mageradaryl12@gmail.com>
2021-10-05 12:40:11 -0400

    added all lat longs for express lane ins and outs

--- src/data.js
+++ src/data.js
@@ -5,8 +5,8 @@ export const eastWestBounds = [
     id: 'a',
     title: 'Daytona Beach/DeLand/Lake Mary',
     coords: [
-      { eLat: 28.460239, eLng: -81.471128 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.712332, eLng: -81.378978 },
+      { wLat: 28.71238, wLng: -81.37876 },
     ],
   },
   {
@@ -14,7 +14,6 @@ export const eastWestBounds = [
     title: 'S.R. 434 (Mile 94)',
     coords: [
       { eLat: 28.69082, eLng: -81.38862 },
-
       { wLat: 28.69071, wLng: -81.3888 },
     ],
   },
@@ -22,9 +21,8 @@ export const eastWestBounds = [
     id: 'c',
     title: 'Central Pkwy. (Mile 93)',
     coords: [
-      { eLat: 28, eLng: -81 },
-
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.67294, eLng: -81.38825 },
+      { wLat: 28.67253, wLng: -81.38893 },
     ],
   },
   {
@@ -40,7 +38,6 @@ export const eastWestBounds = [
     title: 'Maitland Blvd. (S.R. 414) (Mile 90)',
     coords: [
       { eLat: 28.63129, eLng: -81.38746 },
-
       { wLat: 28.63176, wLng: -81.38773 },
     ],
   },
@@ -49,7 +46,6 @@ export const eastWestBounds = [
     title: 'Lee Road (S.R. 423) (Mile 88)',
     coords: [
       { eLat: 28.60602, eLng: -81.38638 },
-
       { wLat: 28.60593, wLng: -81.38659 },
     ],
   },
@@ -65,8 +61,8 @@ export const eastWestBounds = [
     id: 'h',
     title: 'Par St. (Mile 86)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.58241, eLng: -81.37484 },
+      { wLat: 28.5821, wLng: -81.37527 },
     ],
   },
   {
@@ -81,9 +77,8 @@ export const eastWestBounds = [
     id: 'j',
     title: 'Ivanhoe Blvd. (Mile 84)',
     coords: [
-      { eLat: 28.56, eLng: -81.37994 },
-
-      { wLat: 28.55984, wLng: -81.38067 },
+      { eLat: 28.56006, eLng: -81.37982 },
+      { wLat: 28.55982, wLng: -81.38056 },
     ],
   },
   {
@@ -91,7 +86,6 @@ export const eastWestBounds = [
     title: 'Colonial Drive (S.R. 50) (Mile 84)',
     coords: [
       { eLat: 28.55321, eLng: -81.38207 },
-
       { wLat: 28.55298, wLng: -81.38254 },
     ],
   },
@@ -99,96 +93,97 @@ export const eastWestBounds = [
     id: 'l',
     title: 'Amelia St. (Mile 83)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.5494, eLng: -81.38235 },
+      { wLat: 28.54939, wLng: -81.38271 },
     ],
   },
   {
     id: 'm',
     title: 'South St. (Mile 82)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.53847, eLng: -81.38203 },
+      { wLat: 28.53842, wLng: -81.38267 },
     ],
   },
   {
     id: 'n',
     title: 'Anderson St. (Mile 82)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.53627, eLng: -81.38256 },
+      { wLat: 28.53625, wLng: -81.3833 },
     ],
   },
   {
     id: 'o',
     title: 'S.R. 408 (Mile 82)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.53551, eLng: -81.38286 },
+      { wLat: 28.53546, wLng: -81.38365 },
     ],
   },
   {
     id: 'p',
     title: 'Michigan St./Kaley St. (Mile 81)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.51697, eLng: -81.38749 },
+      { wLat: 28.51675, wLng: -81.38813 },
     ],
   },
   {
     id: 'q',
     title: 'Orange Blossom Trl. (US 441/17/92) (Mile 80)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.50989, eLng: -81.39773 },
+      { wLat: 28.51003, wLng: -81.39879 },
     ],
   },
   {
     id: 'r',
     title: 'John Young Pkwy. (Mile 79)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.50846, eLng: -81.4183 },
+      { wLat: 28.50905, wLng: -81.41829 },
     ],
   },
   {
     id: 's',
     title: 'Conroy Road (Mile 78)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.49338, eLng: -81.43298 },
+      { wLat: 28.49361, wLng: -81.43337 },
     ],
   },
   {
     id: 't',
     title: "Florida's Turnpike (Mile 77)",
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.47882, eLng: -81.44896 },
+      { wLat: 28.47919, wLng: -81.44922 },
     ],
   },
   {
     id: 'u',
     title: 'Grand National Drive (Mile 75)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.47341, eLng: -81.45553 },
+      { wLat: 28.4737, wLng: -81.45619 },
     ],
   },
   {
     id: 'v',
     title: 'Kirkman Road (S.R. 435) (Mile 75)',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.47075, eLng: -81.45874 },
+      { wLat: 28.47078, wLng: -81.45961 },
     ],
   },
   {
     id: 'w',
     title: 'Tampa/Lakeland/Kissimmee',
     coords: [
-      { eLat: 0, eLng: -1 },
-      { wLat: 0, wLng: -1 },
+      { eLat: 28.46394, eLng: -81.46678 },
+
+      { wLat: 28.46577, wLng: -81.46537 },
     ],
   },
 ];
