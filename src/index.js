bf0632060c2fa3e71b3eb3173d46ad7a9ec8f76d
Daryl Magera <mageradaryl12@gmail.com>
2021-10-08 15:02:57 -0400

    added exits functionality based on user input

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 // This is set to an environment variable
 const apiKey = process.env.API_KEY;
 
-// Variables
-
 // Contains All the DOM elements
 const domElements = {
   mapContainerStr: document.getElementById('map'),
@@ -17,6 +15,7 @@ const domElements = {
 };
 
 let map;
+let directionValue;
 let locations;
 let marker;
 let infoWindow;
@@ -127,7 +126,7 @@ window.initMap = function () {
 
   // Gets direction from user input East or West
   function getDirection(e) {
-    let directionValue = parseInt(e.target.value, 10);
+    directionValue = parseInt(e.target.value, 10);
     directionValue ? getRoutes(directionValue) : null;
   }
 
@@ -179,13 +178,6 @@ window.initMap = function () {
       boundsIterator(boundPoints);
     }
 
-    // removes all child nodes except the selected one
-    function removeAllChildNodes(parent) {
-      while (!parent.lastChild.id) {
-        parent.removeChild(parent.lastChild);
-      }
-    }
-
     // A function that contains a for..of loop
     function boundsIterator(points) {
       for (boundPoint of points) {
@@ -202,32 +194,7 @@ window.initMap = function () {
 
   // gets respective exits
   function getExits(e) {
-    let boundPoints, selectedPoint, allIds;
-    allIds = [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h',
-      'i',
-      'j',
-      'k',
-      'l',
-      'm',
-      'n',
-      'o',
-      'p',
-      'q',
-      'r',
-      's',
-      't',
-      'u',
-      'v',
-      'w',
-    ];
+    let boundPointsExits, boundPointExit, selectedPoint, exitPointsArr;
     // get the values of each entry point
     selectedPoint = e.target.value;
     // filter the values and return respective exits
@@ -236,8 +203,100 @@ window.initMap = function () {
     // if value === f,g,h,i,j,k,l display t,u,w
     // if value === m display r,t,u,w
     // if value === o,p display t,u,w
-    // if value === p,q,r,s,t display no exit
+    // if value === q,r,s,t display no exit
     // if value === u display w
+    if (directionValue === 2) {
+      switch (selectedPoint) {
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+          exitPointsArr = ['f', 'j', 'm', 'o', 'r', 't', 'u', 'w'];
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+
+        case 'e':
+          exitPointsArr = ['j', 'm', 'o', 'r', 't', 'u', 'w'];
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+
+        case 'f':
+        case 'g':
+        case 'h':
+        case 'i':
+        case 'j':
+        case 'k':
+        case 'l':
+        case 'o':
+        case 'p':
+          exitPointsArr = ['t', 'u', 'w'];
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+
+        case 'm':
+          exitPointsArr = ['r', 't', 'u', 'w'];
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+
+        case 'q':
+        case 'r':
+        case 's':
+        case 't':
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+
+        case 'u':
+          exitPointsArr = ['w'];
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+      }
+    } else {
+      switch (selectedPoint) {
+        case 'w':
+        case 'v':
+        case 'u':
+        case 't':
+        case 's':
+          exitPointsArr = ['p', 'o', 'm', 'e', 'd', 'c', 'a'];
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+
+        case 'r':
+        case 'q':
+        case 'p':
+        case 'o':
+        case 'l':
+        case 'k':
+        case 'i':
+        case 'g':
+        case 'f':
+          exitPointsArr = ['d', 'c', 'a'];
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+
+        case 'n':
+        case 'j':
+          exitPointsArr = ['e', 'd', 'c', 'a'];
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+
+        case 'e':
+        case 'd':
+        case 'b':
+          removeAllChildNodes(domElements.exitPointStr);
+          exitIterator(exitPointsArr);
+          break;
+      }
+    }
 
     // eastbound
     // if value === w,v,u,t,s display p,o,m,e,d,c,a
@@ -245,18 +304,57 @@ window.initMap = function () {
     // if value === n,j display e,d,c,a
     // if value === e,d,b display no exit
     // display exit points in the UI
+
+    // loops and filters exit points, returns matches then injects html code in the dom with the matching data
+    function exitIterator(arr) {
+      if (arr) {
+        boundPointsExits = eastWestBounds.filter(exit => arr.includes(exit.id));
+
+        // reorders the exits based on the direction the user selects i.e
+        // Eastbound/Westbound
+        directionValue === 1 ? boundPointsExits.reverse() : boundPointsExits;
+
+        for (boundPointExit of boundPointsExits) {
+          domElements.exitPointStr.insertAdjacentHTML(
+            'beforeend',
+            `
+          <option value="${boundPointExit.id}">${boundPointExit.title}</option>
+          
+          `
+          );
+        }
+      } else {
+        domElements.exitPointStr.insertAdjacentHTML(
+          'beforeend',
+          `
+          <option>NO EXITS FOUND ):</option>
+          
+          `
+        );
+      }
+
+      console.log(boundPointsExits);
+    }
+
+    function showRoutes() {
+      // created a new instance of the boundary object
+      bounds = new google.maps.LatLngBounds();
+      // Extends the boundaries of the map for each marker
+      markers.forEach(mark => {
+        // console.log(mark);
+        mark.setMap(map);
+        bounds.extend(mark.position);
+      });
+      map.fitBounds(bounds);
+    }
   }
 
-  function showRoutes() {
-    // created a new instance of the boundary object
-    bounds = new google.maps.LatLngBounds();
-    // Extends the boundaries of the map for each marker
-    markers.forEach(mark => {
-      // console.log(mark);
-      mark.setMap(map);
-      bounds.extend(mark.position);
-    });
-    map.fitBounds(bounds);
+  //===========REUSED FUNCTIONS==================//
+  // removes all child nodes except the selected one
+  function removeAllChildNodes(parent) {
+    while (!parent.lastChild.id) {
+      parent.removeChild(parent.lastChild);
+    }
   }
 };
 
